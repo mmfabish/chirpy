10f50a84c62a5003e53cecdb20a248cdd43f3733
Matthew Fabish <mmfabish@gmail.com>
2025-10-25 11:45:40 -0400

    Added health check handler.

--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import (
 	"net/http"
 )
 
+func healthCheckHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", healthCheckHandler)
 
 	server := http.Server{
 		Handler: mux,
